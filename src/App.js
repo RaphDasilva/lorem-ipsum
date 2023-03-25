bceeb0d28bb11f6aa804374412bb3f0bb6e734cc
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-03-25 22:21:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import data from './data';
+import React from 'react';
+import Home from './component/Home';
+
 function App() {
   
   
   return (
-  <h2>lorem ipsum project setup</h2>
+  <Home />
     )
 }
 
